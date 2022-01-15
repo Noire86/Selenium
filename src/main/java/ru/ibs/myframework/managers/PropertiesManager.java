9e86b4f37eb1571ed5990f0f63d1e2985a7de27e
SoularX86 <gleb.iwanow2010@yandex.ru>
2022-01-15 16:57:56 +0500

    PropertiesManager implemented

--- src/main/java/ru/ibs/myframework/managers/PropertiesManager.java
+++ src/main/java/ru/ibs/myframework/managers/PropertiesManager.java
@@ -1,17 +1,54 @@
 package ru.ibs.myframework.managers;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public final class PropertiesManager {
 
 
-    private PropertiesManager(){
+    private final Properties properties = new Properties();
+
+    private PropertiesManager() {
+        loadProperties();
+        loadCustomProperties();
     }
 
-    private static class Holder{
+    private static class Holder {
         public static final PropertiesManager INSTANCE = new PropertiesManager();
     }
 
 
-    public static PropertiesManager getInstance(){
+    public static PropertiesManager getInstance() {
         return Holder.INSTANCE;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+
+    private void loadProperties() {
+        try {
+
+            String path = System.getProperty("propertiesPath", "src/main/resources/");
+            String fileName = System.getProperty("propertiesFilename", "framework");
+
+
+            properties.load(new FileInputStream(path + fileName + ".properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadCustomProperties() {
+        properties.forEach((fileKey, fileValue) ->
+                System.getProperties().forEach((customKey, customValue) -> {
+                            if (fileKey.toString().equals(customKey.toString()) && !fileValue.toString().equals(customValue.toString())) {
+                                properties.setProperty(fileKey.toString(), customValue.toString());
+                            }
+                        }
+                )
+        );
     }
 }
