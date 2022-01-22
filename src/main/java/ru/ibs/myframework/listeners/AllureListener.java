70f8a01b4ef925370386f05535d9acd1ac76457f
SoularX86 <gleb.iwanow2010@yandex.ru>
2022-01-22 20:32:59 +0500

    AllureListener rework

--- src/main/java/ru/ibs/myframework/listeners/AllureListener.java
+++ src/main/java/ru/ibs/myframework/listeners/AllureListener.java
@@ -1,8 +1,9 @@
 package ru.ibs.myframework.listeners;
 
 import io.qameta.allure.Allure;
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import ru.ibs.myframework.managers.DriverManager;
@@ -10,14 +11,13 @@ import ru.ibs.myframework.managers.DriverManager;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class AllureListener implements TestWatcher {
-
+public class AllureListener extends AllureJunit5 implements AfterTestExecutionCallback {
 
     @Override
-    public void testFailed(ExtensionContext context, Throwable cause) {
-        Allure.getLifecycle().addAttachment("FailScr-" + LocalDate.now().toString() + "-" + LocalTime.now().toString(), "image/png", "png", takeScreenshot());
-        DriverManager.getInstance().quitDriver();
-
+    public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
+        if (extensionContext.getExecutionException().isPresent()) {
+            Allure.getLifecycle().addAttachment("FailScr-" + LocalDate.now().toString() + "-" + LocalTime.now().toString(), "image/png", "png", takeScreenshot());
+        }
     }
 
 
